ea4a4f9be79c6685d637a854deb6fa0cff5e5e14
Kash0321 <castillo.sergio@gmail.com>
2017-01-31 11:51:03 +0100

    Migrando la web a ASP.NET Core

--- src/Cinematic.Web/Controllers/SessionsController.cs
+++ src/Cinematic.Web/Controllers/SessionsController.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Cinematic.Domain;
+using System.Net;
+using Cinematic.Domain.Contracts;
+using Cinematic.Web.Models.SessionViewModels;
 
 // For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -10,10 +14,140 @@ namespace Cinematic.Web.Controllers
 {
     public class SessionsController : Controller
     {
-        // GET: /<controller>/
-        public IActionResult Index()
+        IDataContext _dataContext = null;
+
+        public SessionsController(IDataContext dataContext)
+        {
+            _dataContext = dataContext;
+        }
+
+        // GET: Sessions
+        public ActionResult Index(int? page)
+        {
+            var viewModel = new SessionsIndexViewModel();
+            viewModel.PageCount = Math.Ceiling((double)_dataContext.Sessions.Count() / 10);
+
+            if (page.HasValue)
+            {
+                viewModel.HasPrevious = page.Value > 1 ? true : false;
+                viewModel.HasNext = page.Value < viewModel.PageCount ? true : false;
+                viewModel.Sessions = _dataContext.Sessions.Skip((page.Value - 1) * 10).Take(10);
+                viewModel.Page = page.Value;
+            }
+            else
+            {
+                viewModel.HasPrevious = false;
+                viewModel.HasNext = viewModel.PageCount > 1 ? true : false;
+                viewModel.Sessions = _dataContext.Sessions.Skip(0).Take(10);
+                viewModel.Page = 1;
+            }
+
+            return View(viewModel);
+
+        }
+
+        // GET: Sessions/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new StatusCodeResult((int)HttpStatusCode.BadRequest);
+            }
+            Session session = _dataContext.Find<Session>(id);
+            if (session == null)
+            {
+                return NotFound();
+            }
+            return View(session);
+        }
+
+        // GET: Sessions/Create
+        public ActionResult Create()
         {
             return View();
+        }
+
+        // POST: Sessions/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind("Id,TimeAndDate,Status")] Session session)
+        {
+            if (ModelState.IsValid)
+            {
+                _dataContext.Add(session);
+                _dataContext.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(session);
+        }
+
+        // GET: Sessions/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new StatusCodeResult((int)HttpStatusCode.BadRequest);
+            }
+            Session session = _dataContext.Find<Session>(id);
+            if (session == null)
+            {
+                return NotFound();
+            }
+            return View(session);
+        }
+
+        // POST: Sessions/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind("Id,TimeAndDate,Status")] Session session)
+        {
+            if (ModelState.IsValid)
+            {
+                _dataContext.Update(session);
+                _dataContext.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(session);
+        }
+
+        // GET: Sessions/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new StatusCodeResult((int)HttpStatusCode.BadRequest);
+            }
+            Session session = _dataContext.Find<Session>(id);
+            if (session == null)
+            {
+                return NotFound();
+            }
+            return View(session);
+        }
+
+        // POST: Sessions/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Session session = _dataContext.Find<Session>(id);
+            _dataContext.Remove(session);
+            _dataContext.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                _dataContext.Dispose();
+            }
+            base.Dispose(disposing);
         }
     }
 }
---
+++ src/Cinematic.Web/Models/SessionViewModels/SessionsIndexViewModel.cs
@@ -0,0 +1,36 @@
+﻿using Cinematic.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Cinematic.Web.Models.SessionViewModels
+{
+    public class SessionsIndexViewModel
+    {
+        /// <summary>
+        /// Lista de sesiones a renderizar
+        /// </summary>
+        public IEnumerable<Session> Sessions { get; set; }
+
+        /// <summary>
+        /// Página que estamos renderizando
+        /// </summary>
+        public int Page { get; set; }
+
+        /// <summary>
+        /// Número total de páginas
+        /// </summary>
+        public double PageCount { get; set; }
+
+        /// <summary>
+        /// Indica si hay página previa
+        /// </summary>
+        public bool HasPrevious { get; set; }
+
+        /// <summary>
+        /// Inidica si hay página siguiente
+        /// </summary>
+        public bool HasNext { get; set; }
+    }
+}
