4a794e6876d7d8ef50a43ded9b5c62b69867eea2
Kash0321 <castillo.sergio@gmail.com>
2017-02-13 11:56:51 +0100

    Unbound de la carpeta lib de los paquetes bower del SCC

--- src/Cinematic.Web/wwwroot/_references.js
+++ src/Cinematic.Web/wwwroot/_references.js
@@ -2,6 +2,7 @@
 /// <reference path="js/site.js" />
 /// <reference path="js/sitevalidate.js" />
 /// <reference path="lib/bootstrap/dist/js/bootstrap.js" />
+/// <reference path="lib/eonasdan-bootstrap-datetimepicker/build/js/bootstrap-datetimepicker.min.js" />
 /// <reference path="lib/jquery/dist/jquery.js" />
 /// <reference path="lib/jquery-validation/dist/jquery.validate.js" />
 /// <reference path="lib/jquery-validation-unobtrusive/jquery.validate.unobtrusive.js" />
